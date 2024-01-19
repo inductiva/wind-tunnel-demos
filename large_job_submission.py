41917ebbc55f53ec433b047202ff539452e9a222
AugustoPeres <augustoluisperes@gmail.com>
2024-01-19 15:21:23 +0000

    simplifications

--- large_job_submission.py
+++ large_job_submission.py
@@ -8,7 +8,7 @@ from lib import scenarios
 
 import inductiva
 
-INPUT_DATASET = "Path/to/meshes"
+OBJECT_PATH = "metaballs/metaball.obj"
 WORKING_DIR = None
 
 FLOW_VELOCITY_RANGE_X = [20, 30]
@@ -27,36 +27,30 @@ NUM_MACHINES = 1
 DISK_SIZE_GB = 70
 
 
-def run_simulation(object_path, flow_velocity_range_x, flow_velocity_range_y,
-                   flow_velocity_range_z, domain_x, domain_y, domain_z,
-                   num_iterations, resolution, machine_group):
+def run_simulation(machine_group):
     flow_velocity = [
-        np.random.uniform(flow_velocity_range_x[0], flow_velocity_range_x[1]),
-        np.random.uniform(flow_velocity_range_y[0], flow_velocity_range_y[1]),
-        np.random.uniform(flow_velocity_range_y[0], flow_velocity_range_z[1]),
+        np.random.uniform(FLOW_VELOCITY_RANGE_X[0], FLOW_VELOCITY_RANGE_X[1]),
+        np.random.uniform(FLOW_VELOCITY_RANGE_Y[0], FLOW_VELOCITY_RANGE_Y[1]),
+        np.random.uniform(FLOW_VELOCITY_RANGE_Y[0], FLOW_VELOCITY_RANGE_Z[1]),
     ]
     wind_tunnel = models.WindTunnel(flow_velocity=flow_velocity,
-                                    x_min=domain_x[0],
-                                    x_max=domain_x[1],
-                                    y_min=domain_y[0],
-                                    y_max=domain_y[1],
-                                    z_min=domain_z[0],
-                                    z_max=domain_z[1])
+                                    x_min=DOMAIN_X[0],
+                                    x_max=DOMAIN_X[1],
+                                    y_min=DOMAIN_Y[0],
+                                    y_max=DOMAIN_Y[1],
+                                    z_min=DOMAIN_Z[0],
+                                    z_max=DOMAIN_Z[1])
 
     sim_parameters = scenarios.SimulationParameters(
-        num_iterations=num_iterations, resolution=resolution)
+        num_iterations=NUM_ITERATIONS, resolution=RESOLUTION)
 
     scenario = scenarios.WindTunnelScenario(wind_tunnel=wind_tunnel)
-    scenario.simulate(object_path=object_path,
+    scenario.simulate(object_path=OBJECT_PATH,
                       sim_params=sim_parameters,
                       on=machine_group)
 
 
 def main():
-    object_paths = [
-        os.path.join(INPUT_DATASET, path) for path in os.listdir(INPUT_DATASET)
-    ]
-
     if WORKING_DIR is not None:
         inductiva.working_dir = WORKING_DIR
 
@@ -65,12 +59,8 @@ def main():
                                                      disk_size_gb=DISK_SIZE_GB)
     machine_group.start()
 
-    for object_path in object_paths:
-        for _ in range(NUM_SIMULATIONS_PER_OBJECT):
-            run_simulation(object_path, FLOW_VELOCITY_RANGE_X,
-                           FLOW_VELOCITY_RANGE_Y, FLOW_VELOCITY_RANGE_Z,
-                           DOMAIN_X, DOMAIN_Y, DOMAIN_Z, NUM_ITERATIONS,
-                           RESOLUTION, machine_group)
+    for _ in range(NUM_SIMULATIONS_PER_OBJECT):
+        run_simulation(machine_group)
 
 
 if __name__ == "__main__":
